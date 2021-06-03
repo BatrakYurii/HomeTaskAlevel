208fa68b3e202d66326e0a60bdae998751ffc437
Batrak Yurii <navid7498@gmail.com>
2021-06-03 17:16:54 +0300

    hashtable

---
+++ HashNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master
+{
+    public class HashNode<K, T> where K : IComparable<K> where T : IComparable<T>
+    {
+        private K _key;
+        private T _value;
+        private HashNode<K, T> _next;
+        private bool _isDeleted;
+
+        public K Key => _key;
+
+        public T Value
+        {
+            get => _value;
+            set => _value = value;
+        }
+
+        public bool IsDeleted
+        {
+            get { return _isDeleted; }
+            set { _isDeleted = value; }
+        }
+
+        public HashNode<K, T> Next
+        {
+            set => _next = value;
+            get => _next;
+        }
+
+        public HashNode(K key, T value)
+        {
+            _key = key;
+            _value = value;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,19 @@
 
 namespace Master
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var h = new HashTable<int, string>();
+            h.Add(33, "First element");
+            h.Add(44, "Second element");
+            h.Add(33, "Third element");
+
+            Console.WriteLine(h.Find(33));
+
+            h.Remove(33);
+            h.ContainsValue("cond element");
         }
     }
 }

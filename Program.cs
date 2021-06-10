067ac08aa6741f8f00642da5709572265817ea4f
Batrak Yurii <navid7498@gmail.com>
2021-06-10 17:12:56 +0300

    1 Commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,28 @@
 
 namespace Master
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Component animals = new Subtree("animals");
+
+            Component mammals = new Subtree("mammals");
+            animals.Add(mammals);
+
+            Component whale = new Leaf("whale");
+            mammals.Add(whale);
+
+            Component insects = new Leaf("insects");
+            animals.Add(insects);
+
+            animals.Print();
+
+            Console.WriteLine("---------------------");
+
+            mammals.Print();
+
+
         }
     }
 }

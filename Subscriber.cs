239292582ef9a6deb087bb675cfde7b6bec96029
Batrak Yurii <navid7498@gmail.com>
2021-08-19 16:31:43 +0300

    Simple implementation Pub-Sub pattern

---
+++ Subscriber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Subscriber
+{
+    public string Name { get; set; }
+    public Subscriber(string name)
+    {
+        Name = name; 
+    }
+
+    public void Subscribe(Publisher publisher)
+    {
+        publisher.myEvent += Update;
+    }
+
+    public void Unsubscribe(Publisher publisher)
+    {
+        publisher.myEvent -= Update;
+    }
+
+    public void Update(object sender, EventArguments args)
+    {
+        Publisher publisher = (Publisher)sender;
+        Console.WriteLine($"{publisher.Name} send a message : {args.Message}");
+    }
+}
